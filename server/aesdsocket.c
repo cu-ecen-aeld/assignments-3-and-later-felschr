0cbfce9373684be9ff912c8bc1eeec3a513f35d4
Felix Schröter <dev@felschr.com>
2025-01-22 18:54:25 +0100

    add daemon mode to server/aesdsocket.c

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,9 +1,12 @@
+#include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <syslog.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
 
 #define PORT 9000
@@ -12,12 +15,47 @@
 
 int running = 1;
 
-void signal_handler(int signum) {
+void signal_handler() {
     syslog(LOG_INFO, "Caught signal, exiting");
     running = 0;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+  int daemon_mode = 0;
+  for (int i = 1; i < argc; i++) {
+      if (strcmp(argv[i], "-d") == 0) {
+          daemon_mode = 1;
+      }
+  }
+
+  if (daemon_mode) {
+      pid_t pid = fork();
+      if (pid < 0) {
+          syslog(LOG_ERR, "Fork failed");
+          return 1;
+      }
+      if (pid > 0) {
+        return 1;
+      }
+      umask(0);
+      if (chdir("/") < 0) {
+          syslog(LOG_ERR, "Chdir failed");
+          return 1;
+      }
+      close(STDIN_FILENO);
+      close(STDOUT_FILENO);
+      close(STDERR_FILENO);
+  }
+
+  if (signal(SIGINT, signal_handler) == SIG_ERR) {
+      syslog(LOG_ERR, "Failed to set SIGINT handler");
+      return 1;
+  }
+  if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+      syslog(LOG_ERR, "Failed to set SIGTERM handler");
+      return 1;
+  }
+
   int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   struct sockaddr_in serv_addr;
